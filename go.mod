546d6224fd802a447cde9f8a756d2fef66b8b9af
Ricardo Branco <rbranco@suse.de>
2025-12-01 22:08:53 +0100

    Define some constants ourselves to drop dependency

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ricardobranco777/go-libarchive
 
 go 1.25.0
-
-require golang.org/x/sys v0.38.0 // indirect
--- strmode.go
+++ strmode.go
@@ -4,8 +4,29 @@ package archive
 
 import (
 	"io/fs"
+)
 
-	"golang.org/x/sys/unix"
+const (
+	S_IFMT   = 0xf000
+	S_IFSOCK = 0xc000
+	S_IFLNK  = 0xa000
+	S_IFREG  = 0x8000
+	S_IFBLK  = 0x6000
+	S_IFDIR  = 0x4000
+	S_IFCHR  = 0x2000
+	S_IFIFO  = 0x1000
+	S_ISUID  = 0x800
+	S_ISGID  = 0x400
+	S_ISVTX  = 0x200
+	S_IRUSR  = 0x100
+	S_IWUSR  = 0x80
+	S_IXUSR  = 0x40
+	S_IRGRP  = 0x20
+	S_IWGRP  = 0x10
+	S_IXGRP  = 0x8
+	S_IROTH  = 0x4
+	S_IWOTH  = 0x2
+	S_IXOTH  = 0x1
 )
 
 // Converts fs.FileMode to uint32
@@ -18,32 +39,32 @@ func FileModeToMode(m fs.FileMode) uint32 {
 	// type
 	switch {
 	case m.IsDir():
-		mode |= unix.S_IFDIR
+		mode |= S_IFDIR
 	case m&fs.ModeSymlink != 0:
-		mode |= unix.S_IFLNK
+		mode |= S_IFLNK
 	case m&fs.ModeNamedPipe != 0:
-		mode |= unix.S_IFIFO
+		mode |= S_IFIFO
 	case m&fs.ModeSocket != 0:
-		mode |= unix.S_IFSOCK
+		mode |= S_IFSOCK
 	case m&fs.ModeDevice != 0:
 		if m&fs.ModeCharDevice != 0 {
-			mode |= unix.S_IFCHR
+			mode |= S_IFCHR
 		} else {
-			mode |= unix.S_IFBLK
+			mode |= S_IFBLK
 		}
 	default:
-		mode |= unix.S_IFREG
+		mode |= S_IFREG
 	}
 
 	// special bits
 	if m&fs.ModeSetuid != 0 {
-		mode |= unix.S_ISUID
+		mode |= S_ISUID
 	}
 	if m&fs.ModeSetgid != 0 {
-		mode |= unix.S_ISGID
+		mode |= S_ISGID
 	}
 	if m&fs.ModeSticky != 0 {
-		mode |= unix.S_ISVTX
+		mode |= S_ISVTX
 	}
 
 	return mode
@@ -58,88 +79,88 @@ func StrMode(m fs.FileMode) string {
 	mode := FileModeToMode(m)
 
 	// File type
-	switch mode & unix.S_IFMT {
-	case unix.S_IFDIR:
+	switch mode & S_IFMT {
+	case S_IFDIR:
 		b[0] = 'd'
-	case unix.S_IFCHR:
+	case S_IFCHR:
 		b[0] = 'c'
-	case unix.S_IFBLK:
+	case S_IFBLK:
 		b[0] = 'b'
-	case unix.S_IFREG:
+	case S_IFREG:
 		b[0] = '-'
-	case unix.S_IFLNK:
+	case S_IFLNK:
 		b[0] = 'l'
-	case unix.S_IFSOCK:
+	case S_IFSOCK:
 		b[0] = 's'
-	case unix.S_IFIFO:
+	case S_IFIFO:
 		b[0] = 'p'
 	default:
 		b[0] = '?'
 	}
 
 	// User permissions
-	if mode&unix.S_IRUSR != 0 {
+	if mode&S_IRUSR != 0 {
 		b[1] = 'r'
 	} else {
 		b[1] = '-'
 	}
-	if mode&unix.S_IWUSR != 0 {
+	if mode&S_IWUSR != 0 {
 		b[2] = 'w'
 	} else {
 		b[2] = '-'
 	}
-	switch mode & (unix.S_IXUSR | unix.S_ISUID) {
+	switch mode & (S_IXUSR | S_ISUID) {
 	case 0:
 		b[3] = '-'
-	case unix.S_IXUSR:
+	case S_IXUSR:
 		b[3] = 'x'
-	case unix.S_ISUID:
+	case S_ISUID:
 		b[3] = 'S'
-	case unix.S_IXUSR | unix.S_ISUID:
+	case S_IXUSR | S_ISUID:
 		b[3] = 's'
 	}
 
 	// Group permissions
-	if mode&unix.S_IRGRP != 0 {
+	if mode&S_IRGRP != 0 {
 		b[4] = 'r'
 	} else {
 		b[4] = '-'
 	}
-	if mode&unix.S_IWGRP != 0 {
+	if mode&S_IWGRP != 0 {
 		b[5] = 'w'
 	} else {
 		b[5] = '-'
 	}
-	switch mode & (unix.S_IXGRP | unix.S_ISGID) {
+	switch mode & (S_IXGRP | S_ISGID) {
 	case 0:
 		b[6] = '-'
-	case unix.S_IXGRP:
+	case S_IXGRP:
 		b[6] = 'x'
-	case unix.S_ISGID:
+	case S_ISGID:
 		b[6] = 'S'
-	case unix.S_IXGRP | unix.S_ISGID:
+	case S_IXGRP | S_ISGID:
 		b[6] = 's'
 	}
 
 	// Other permissions
-	if mode&unix.S_IROTH != 0 {
+	if mode&S_IROTH != 0 {
 		b[7] = 'r'
 	} else {
 		b[7] = '-'
 	}
-	if mode&unix.S_IWOTH != 0 {
+	if mode&S_IWOTH != 0 {
 		b[8] = 'w'
 	} else {
 		b[8] = '-'
 	}
-	switch mode & (unix.S_IXOTH | unix.S_ISVTX) {
+	switch mode & (S_IXOTH | S_ISVTX) {
 	case 0:
 		b[9] = '-'
-	case unix.S_IXOTH:
+	case S_IXOTH:
 		b[9] = 'x'
-	case unix.S_ISVTX:
+	case S_ISVTX:
 		b[9] = 'T'
-	case unix.S_IXOTH | unix.S_ISVTX:
+	case S_IXOTH | S_ISVTX:
 		b[9] = 't'
 	}
 
